4bd9c8d2a694e3555d7ea11d6052c77497f80558
Gichohi-Simon <njogugichohi56@gmail.com>
2025-10-29 15:41:34 +0300

    added .js in product controller

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import cloudinary from "../utils/cloudinaryConfig";
+import cloudinary from "../utils/cloudinaryConfig.js";
 import fs from "fs";
 
 const prisma = new PrismaClient();
