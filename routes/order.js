da4a359c03e0816f8fc7c148695515ae5a04b1c5
Gichohi-Simon <njogugichohi56@gmail.com>
2026-02-04 21:25:34 +0300

    order route updated

--- routes/order.js
+++ routes/order.js
@@ -11,36 +11,53 @@ import {
   markOrderAsPaid,
   getSingleOrderById,
 } from "../controllers/orderController.js";
+
 import {
   authenticate,
-  authorizeAdmin,
+  authorizeAdminOrEmployee,
 } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
 router.post("/create-order", authenticate, createOrder);
-router.get("/allOrders", authenticate, authorizeAdmin, getAllOrders);
-router.get("/total-orders", authenticate, authorizeAdmin, getTotalOrders);
-router.get("/total-sales", authenticate, authorizeAdmin, getTotalSales);
+
+router.get("/allOrders", authenticate, authorizeAdminOrEmployee, getAllOrders);
+router.get(
+  "/total-orders",
+  authenticate,
+  authorizeAdminOrEmployee,
+  getTotalOrders,
+);
+router.get(
+  "/total-sales",
+  authenticate,
+  authorizeAdminOrEmployee,
+  getTotalSales,
+);
+
 router.get("/get-current-user-orders", authenticate, getCurrentUserOrders);
+
 router.get("/invoice/:orderId", authenticate, getInvoice);
 router.get("/get-user-order-by-id/:orderId", authenticate, getSingleOrderById);
+
 router.get(
   "/singleUser-orders/:id",
   authenticate,
-  authorizeAdmin,
+  authorizeAdminOrEmployee,
   getSingleUserOrders,
 );
+
 router.patch(
   "/mark-as-delivered/:orderId",
   authenticate,
-  authorizeAdmin,
+  authorizeAdminOrEmployee,
   markOrderAsDelivered,
 );
+
 router.patch(
   "/mark-as-paid/:orderId",
   authenticate,
-  authorizeAdmin,
+  authorizeAdminOrEmployee,
   markOrderAsPaid,
 );
 
