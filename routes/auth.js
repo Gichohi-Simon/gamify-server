90bdfb11861bd0e24c6466fdacb13c9adc37b328
Gichohi-Simon <njogugichohi56@gmail.com>
2026-02-04 11:31:29 +0300

    added google oauth to route

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,14 @@ import {
   login,
   logoutCurrentUser,
   signUp,
+  googleLogin,
 } from "../controllers/authController.js";
 const router = express.Router();
 
 router.get("/check", checkCookie);
 router.post("/login", login);
 router.post("/sign-up", signUp);
+router.post("/google", googleLogin);
 router.post("/logout", logoutCurrentUser);
 
 export default router;
